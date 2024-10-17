0a28e37f78daeadeaddf4ec09f253cbc62f77fd0
Jimmy Lee <chiminglee831@tamu.edu>
2024-10-17 14:26:42 -0500

    email function finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -131,22 +131,10 @@ if Rails.env.production?
         invitation_accepted_at: Time.zone.now
     )
 
-    # contact_person_2 = FactoryBot.create(
-    #     :user,
-    #     email: 'brightna00b@gmail.com',
-    #     password: 'password',
-    #     first_name: 'brightna00b',
-    #     last_name: 'brightna00b',
-    #     level: UserLevel::THREE,
-    #     program: Program.first,
-    #     invitation_accepted_at: Time.zone.now
-    # )
-
-
     # contact_person = User.find_by(email: 'user@example.com')
     # Create some documents with nearby expiries to test expiring docs mailer
     statuses = ContractStatus.list.reject { |status| status == :created }
-    (1..30).each do |i|
+    (1..100).each do |i|
         d = Time.zone.today + 1.day * i
         FactoryBot.create(
             :contract,
@@ -164,25 +152,6 @@ if Rails.env.production?
             contract_status: statuses.sample
         )
     end
-
-    # (1..30).each do |i|
-    #     d = Time.zone.today + 1.day * i
-    #     FactoryBot.create(
-    #         :contract,
-    #         id: 80 + i,
-    #         point_of_contact: contact_person_2,    
-    #         title: "Expiry Contract_2 #{i}",
-    #         program: Program.all.sample,
-    #         vendor: Vendor.all.sample,
-    #         entity: Entity.all.sample,
-    #         ends_at: d,
-    #         ends_at_final: d + 1.day * i,
-    #         extension_count: i,
-    #         extension_duration: i,
-    #         extension_duration_units: TimePeriod::MONTH,
-    #         contract_status: statuses.sample
-    #     )
-    # end
 
     BvcogConfig.create(
         contracts_path: Rails.root.join('public/contracts'),
