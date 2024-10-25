dc778db79de41a9fa9aec2ebea9a290205d6677a
Jimmy Lee <chiminglee831@tamu.edu>
2024-10-25 01:10:02 -0500

    test-1

--- app/enumerations/contract_type.rb
+++ app/enumerations/contract_type.rb
@@ -14,13 +14,13 @@ class ContractType < EnumerateIt::Base
     )
 
     OPTIONS = {
-        contract: "Contract",
-        grant: "Grant",
-        property_lease: "Property Lease",
-        ila: "ILA",
-        mou_moa: "MOU/MOA",
-        letter_of_intent: "Letter of Intent",
-        amendment: "Amendment",
-        other: "Other"
-      }
+        contract: 'Contract',
+        grant: 'Grant',
+        property_lease: 'Property Lease',
+        ila: 'ILA',
+        mou_moa: 'MOU/MOA',
+        letter_of_intent: 'Letter of Intent',
+        amendment: 'Amendment',
+        other: 'Other'
+    }.freeze
 end
