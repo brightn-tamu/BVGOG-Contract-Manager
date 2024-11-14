ef7b5a2117723a80b042af90edf102e5c3c929c1
jingruimao <vivinmao@tamu.edu>
2024-11-13 21:03:26 -0600

    add pop window

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -466,25 +466,6 @@ class ContractsController < ApplicationController
                     }
 
                     if ModificationLog.create(log_attributes)
-<<<<<<< HEAD
-                      @contract.update(contract_status: ContractStatus::IN_PROGRESS)
-                      @contract.update(current_type: source_page)
-                      format.html do
-                        # erase the session value after successful creation of contract
-                        # so that the value of the dropdowns will not be retained for the next contract creation
-                        session[:value_type] = nil
-                        session[:vendor_visible_id] = nil
-                        success_message = case source_page
-                                          when "renew"
-                                            "Renewal request for #{@contract.title} submitted successfully and is pending approval."
-                                          when "amend"
-                                            "Amendment request for #{@contract.title} submitted successfully and is pending approval"
-                                          else
-                                            "error"
-                                          end
-                        redirect_to send("modify_contracts_path", @contract), notice: success_message
-                      end
-=======
                         @contract.update(contract_status: ContractStatus::IN_PROGRESS)
                         @contract.update(current_type: source_page)
                         format.html do
@@ -506,7 +487,6 @@ class ContractsController < ApplicationController
                                 notice: success_message
                             )
                         end
->>>>>>> c7e828415f4fa5379314051f8d48602e2f352c1b
                     else
                         render source_page, alert: 'Failed to update TempContract.'
                     end
@@ -523,22 +503,7 @@ class ContractsController < ApplicationController
                         # so that the value of the dropdowns will not be retained for the next contract creation
                         session[:value_type] = nil
                         session[:vendor_visible_id] = nil
-<<<<<<< HEAD
                         redirect_to edit_contract_path(@contract), notice: "Contract was successfully updated."
-=======
-                        session[:funding_source] = nil 
-
-                        success_message = case source_page
-                                          when 'renew'
-                                              "Renewal request for #{@contract.title} submitted successfully and is pending approval."
-                                          when 'amend'
-                                              "Amendment request for #{@contract.title} submitted successfully and is pending approval"
-                                          else
-                                              'Contract was successfully updated.'
-                                          end
-                        redirect_to contract_path(@contract), notice: success_message
-                    end
->>>>>>> c7e828415f4fa5379314051f8d48602e2f352c1b
                     format.json { render :show, status: :ok, location: @contract }
                     end
                 else
@@ -851,8 +816,4 @@ def get_file
 
         contract_params[:total_amount]
     end
-<<<<<<< HEAD
 end
-
-=======
->>>>>>> c7e828415f4fa5379314051f8d48602e2f352c1b
