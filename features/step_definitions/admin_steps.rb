8e890be3081642633fafa09f8008d346d4d7a1cd
JingyuanMu <mjy333@tamu.edu>
2024-11-23 02:59:35 +0800

    fix tests issur

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-When('I check the program {int} check box') do |_int|
-    checkbox = find('#bvcog_config_delete_programs_1', visible: false)
-    checkbox.check
+When('I press the program {int} delete button') do |program_id|
+  visit "/admin/delete_program?program_id=#{program_id}"
 end
 
-When('I check the entity {int} check box') do |_int|
-    checkbox = find('#bvcog_config_delete_entities_1', visible: false)
-    checkbox.check
+When('I press the entity {int} delete button') do |entity_id|
+    visit "/admin/delete_entity?entity_id=#{entity_id}"
 end
