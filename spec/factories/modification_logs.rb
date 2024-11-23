5263253d90e4d59cc33865fa6ea8312f867015f1
Zhaoming-tamu <zhaoming0812@tamu.edu>
2024-11-23 02:40:30 +0000

    rspec 94.7

--- spec/factories/modification_logs.rb
+++ spec/factories/modification_logs.rb
@@ -2,5 +2,32 @@
 
 FactoryBot.define do
     factory :modification_log do
+		contract
+		status { 'pending' }
+		modification_type { 'amend' }
+		modified_by { association :user }
+
+		transient do
+			old_number { Faker::Alphanumeric.alphanumeric(number: 10) }
+			new_number { Faker::Alphanumeric.alphanumeric(number: 10) }
+			old_starts_at { Faker::Date.between(from: 2.years.ago, to: Time.zone.today).to_s }
+			new_starts_at { Faker::Date.between(from: 2.years.ago, to: Time.zone.today).to_s }
+			old_ends_at { Faker::Date.between(from: 20.years.from_now, to: 30.years.from_now).to_s }
+			new_ends_at { Faker::Date.between(from: 20.years.from_now, to: 30.years.from_now).to_s }
+			old_total_amount { Faker::Number.between(from: 0, to: 99_000_000).to_s }
+			new_total_amount { Faker::Number.between(from: 0, to: 99_000_000).to_s }
+			old_description { Faker::Lorem.paragraph(sentence_count: 15) }
+			new_description { Faker::Lorem.paragraph(sentence_count: 15) }
+		end
+
+		changes_made {
+			{
+				'number' => [old_number, new_number],
+				'starts_at' => [old_starts_at, new_starts_at],
+				'ends_at' => [old_ends_at, new_ends_at],
+				'total_amount' => [old_total_amount, new_total_amount],
+				'description' => [old_description, new_description]
+			}
+		}
     end
 end
--- spec/views/contracts/show.html.erb_spec.rb
+++ spec/views/contracts/show.html.erb_spec.rb
@@ -4,100 +4,99 @@ require 'rails_helper'
 require 'auth_helper'
 
 RSpec.describe 'contracts/show', type: :view do
-    include Devise::Test::IntegrationHelpers
-    include Devise::Test::ControllerHelpers
-    include FactoryBot::Syntax::Methods
+	include Devise::Test::IntegrationHelpers
+	include Devise::Test::ControllerHelpers
+	include FactoryBot::Syntax::Methods
 
-    before do
-        login_user
-        vendor = create(:vendor)
-        entity = create(:entity)
-        program = create(:program)
-        point_of_contact = create(:user)
-      
-        @contract = assign(:contract, create(
-            :contract,
-            vendor: vendor,
-            entity: entity,
-            program: program,
-            point_of_contact: point_of_contact,
-            contract_status: ContractStatus::IN_PROGRESS,
-        ))
-        user = create(:user)
-        @modification_log = create(
-          :modification_log,
-          contract: @contract,
-          status: 'pending',
-          changes_made: {
-            'number' => ['123', '456'],
-            'starts_at' => ['2023-01-01', '2024-01-01'],
-            'ends_at' => ['2023-12-31', '2024-12-31'],
-            'total_amount' => [1000, 1500],
-            'description' => ['Old Description', 'New Description']
-          },
-          modified_by: user,
-          modification_type: 'amend',
-        )
-        render
-    end
+	before do
+		login_user
+		@vendor = create(:vendor)
+		@entity = create(:entity)
+		@program = create(:program)
+		@point_of_contact = create(:user)
 
+		@contract = assign(:contract, create(
+			:contract,
+			vendor: @vendor,
+			entity: @entity,
+			program: @program,
+			point_of_contact: @point_of_contact,
+			contract_status: ContractStatus::IN_PROGRESS
+		))
 
-    it 'renders the contract title' do
-        expect(rendered).to include(@contract.title)
-    end
-    
-    it 'renders the contract ID with modification log differences' do
-        expect(rendered).to include('123')
-        expect(rendered).to include('456')
-    end
-    
-    it 'renders the contract type' do
-        expect(rendered).to include(@contract.contract_type_humanize)
-    end
-    
-    it 'renders the point of contact link' do
-        expect(rendered).to include(user_path(@contract.point_of_contact))
-        expect(rendered).to include(@contract.point_of_contact.full_name)
-    end
-    
-    it 'renders the vendor link' do
-        expect(rendered).to include(vendor_path(@contract.vendor))
-        expect(rendered).to include(@contract.vendor.name)
-    end
-    
-    it 'renders the funding source, or N/A if not present' do
-        if @contract.funding_source.present?
-          expect(rendered).to include(@contract.funding_source)
-        else
-          expect(rendered).to include('N/A')
-        end
-    end
-    
-    it 'renders the entity name' do
-        expect(rendered).to include(@contract.entity.name)
-    end
-    
-    it 'renders the program name' do
-        expect(rendered).to include(@contract.program.name)
-    end
-    
-    it 'renders the start date with modification log differences' do
-        expect(rendered).to include('January 01, 2023')
-        expect(rendered).to include('January 01, 2024')
-    end
-    
-    it 'renders the end date with modification log differences' do
-        expect(rendered).to include('December 31, 2023')
-        expect(rendered).to include('December 31, 2024')
-    end
-    
-    it 'renders the contract value with modification log differences' do
-        expect(rendered).to include('1,000.00')
-        expect(rendered).to include('1,500.00')
-    end
-    
-    it 'renders the description with modification log differences' do
-        expect(rendered).to include('Old Description')
-        expect(rendered).to include('New Description')
-    end
+		@modification_log = create(
+			:modification_log,
+			contract: @contract
+		)
+
+		render
+	end
+
+	it 'renders the contract title' do
+		expect(rendered).to include(@contract.title)
+	end
+
+	it 'renders the contract ID with modification log differences' do
+		old_number = @modification_log.changes_made['number'].first
+		new_number = @modification_log.changes_made['number'].last
+		expect(rendered).to include(old_number)
+		expect(rendered).to include(new_number)
+	end
+
+	it 'renders the contract type' do
+		expect(rendered).to include(@contract.current_type)
+	end
+
+	it 'renders the point of contact link' do
+		point_of_contact_name = "#{@point_of_contact.first_name} #{@point_of_contact.last_name}"
+		expect(rendered).to include(point_of_contact_name)
+	  end
+
+	it 'renders the vendor link' do
+		expect(rendered).to include(@vendor.name)
+	end
+
+	it 'renders the funding source, or N/A if not present' do
+		if @contract.funding_source.present?
+		expect(rendered).to include(@contract.funding_source)
+		else
+		expect(rendered).to include('N/A')
+		end
+	end
+
+	it 'renders the entity name' do
+		expect(rendered).to include(@entity.name)
+	end
+
+	it 'renders the program name' do
+		expect(rendered).to include(@program.name)
+	end
+
+	it 'renders the start date with modification log differences' do
+		old_starts_at = Date.parse(@modification_log.changes_made['starts_at'].first).strftime("%B %d, %Y")
+		new_starts_at = Date.parse(@modification_log.changes_made['starts_at'].last).strftime("%B %d, %Y")
+		expect(rendered).to include(old_starts_at)
+		expect(rendered).to include(new_starts_at)
+	end
+
+	it 'renders the end date with modification log differences' do
+		old_ends_at = Date.parse(@modification_log.changes_made['ends_at'].first).strftime("%B %d, %Y")
+		new_ends_at = Date.parse(@modification_log.changes_made['ends_at'].last).strftime("%B %d, %Y")
+		expect(rendered).to include(old_ends_at)
+		expect(rendered).to include(new_ends_at)
+	end
+
+	it 'renders the contract value with modification log differences' do
+		old_total_amount = number_to_currency(@modification_log.changes_made['total_amount'].first)
+		new_total_amount = number_to_currency(@modification_log.changes_made['total_amount'].last)
+		expect(rendered).to include(old_total_amount)
+		expect(rendered).to include(new_total_amount)
+	end
+
+	it 'renders the description with modification log differences' do
+		old_description = @modification_log.changes_made['description'].first
+		new_description = @modification_log.changes_made['description'].last
+		expect(rendered).to include(old_description)
+		expect(rendered).to include(new_description)
+	end
 end
